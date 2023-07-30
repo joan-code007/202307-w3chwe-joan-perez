27a9da7ea69f0cddc191128b752350adca965628
Joan Pérez Lapuente <joanperez@MacBook-Pro-de-Joan.local>
2023-07-30 19:31:25 +0200

    Add interface componentsStructure in types

--- src/ts/globals/globals.ts
+++
@@ -1,3 +0,0 @@
-const linkPokeApi = "https://pokeapi.co/api/v2/pokemon/?limits=15";
-
-export default linkPokeApi;
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -2,3 +2,8 @@ export interface PokemonData {
   name: string;
   url: string;
 }
+
+export interface ComponentStructure {
+  render: () => void;
+  remove: () => void;
+}
