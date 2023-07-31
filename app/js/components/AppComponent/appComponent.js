0c4cee371ac7161e7bf94c7e6ae492ace4e4f470
Joan Pérez Lapuente <joanperez@MacBook-Pro-de-Joan.local>
2023-07-31 02:33:01 +0200

    Add AppComponent.ts

---
+++ app/js/components/AppComponent/appComponent.js
@@ -0,0 +1,7 @@
+import Component from "../Component/component.js";
+class AppComponent extends Component {
+    constructor(parentElement) {
+        super(parentElement, "div", "app");
+    }
+}
+export default AppComponent;
---
+++ src/ts/components/AppComponent/appComponent.ts
@@ -0,0 +1,8 @@
+import Component from "../Component/component.js";
+class AppComponent extends Component {
+  constructor(parentElement: Element) {
+    super(parentElement, "div", "app");
+  }
+}
+
+export default AppComponent;
